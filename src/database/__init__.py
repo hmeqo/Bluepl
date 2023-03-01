5e17e803c896db927b0037a64d5ca5c4379dae9f
hmeqo <hmeqo@qq.com>
2023-03-01 09:45:28 +0800

    remove oll file

--- src/database/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-数据库, 内置多种数据库实现, 应按照 `.dbapi.py` 中所规定的接口配置数据库接口
-需要调用数据库实现模块中 `init` 函数初始化数据
-使用 set_dbapi 设置数据库接口
-使用 get_dbapi 获取数据库接口
-"""
-
-from .dbapi import set_dbapi, get_dbapi
--- src/initialize.py
+++ src/initialize.py
@@ -1,6 +1,5 @@
 import typing as _t
 
-from .database.dbapi import DBApi, set_dbapi
 from .event import EventType, emit
 from . import gconfig
 
